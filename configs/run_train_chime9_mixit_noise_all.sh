8bd629db979f64ccf5025cb985e5f9428ad121d6
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2025-12-24 21:37:31 +0900

    Refactor data loading and processing in dataLoader_chime9_mixit.py; improve handling of audio and lip crop segments, and update training script to adjust parameters for better performance.

---
+++ configs/run_train_chime9_mixit_noise_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+printf "seanet-CHIME9-MixIT-training (all session pairs, noise) \n"
+CUDA_VISIBLE_DEVICES=5 python ../main_chime9_mixit_noise_all.py \
+--save_path exps/seanet_chime9_mixit_noise_all \
+--data_list /net/midgar/work/nitsu/work/chime9/data/datalist_train \
+--backbone seanet \
+--n_cpu 4 \
+--length 3 \
+--batch_size 1 \
+--max_epoch 150 \
+--lr 0.00100 \
+--alpha 1.0 \
+--val_step 3 \
+--lr_decay 0.97
+
