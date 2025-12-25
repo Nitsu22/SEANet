c1f6d68079febbd66bcc34fed6f3bba7786ffd96
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2025-12-25 23:57:46 +0900

    Remove deprecated training scripts and configuration files for MixIT model; eliminate main_chime9_mixit_noise_all.py, main_chime9_mixit_noise_norest_all.py, trainer_chime9_mixit_noise_norest.py, trainer_chime9_mixit_noise.py, and associated shell scripts to streamline the codebase.

--- configs/run_train_chime9_mixit_all_half_ft.sh
+++ configs/run_train_chime9_mixit_all_half_ft.sh
@@ -2,9 +2,9 @@
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-printf "seanet-CHIME9-MixIT-training (all session pairs, noise) \n"
-CUDA_VISIBLE_DEVICES=5 python ../main_chime9_mixit_noise_all.py \
---save_path exps/seanet_chime9_mixit_noise_all \
+printf "seanet-CHIME9-MixIT-training (all session pairs) \n"
+CUDA_VISIBLE_DEVICES=5 python ../main_chime9_mixit_all.py \
+--save_path exps/seanet_chime9_mixit_all \
 --data_list /net/midgar/work/nitsu/work/chime9/data/datalist_train \
 --backbone seanet \
 --n_cpu 4 \
@@ -12,8 +12,7 @@ CUDA_VISIBLE_DEVICES=5 python ../main_chime9_mixit_noise_all.py \
 --batch_size 1 \
 --max_epoch 150 \
 --lr 0.00100 \
---alpha 0.10 \
+--alpha 1.0 \
 --val_step 3 \
 --lr_decay 0.97 \
 --init_model /net/midgar/work/nitsu/work/chime9/SEANet/exps/seanet/model/model_0147.model
-
