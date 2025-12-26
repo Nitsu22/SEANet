ebc37ab82d80add82fc4338f84516f800724c874
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2025-12-26 14:56:26 +0900

    Update CUDA device setting in training script to use device 0 for CHIME9 MixIT training; ensure proper execution environment configuration.

--- configs/run_train_chime9_mixit_all_half_ft.sh
+++ configs/run_train_chime9_mixit_all_half_ft.sh
@@ -3,7 +3,7 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 printf "seanet-CHIME9-MixIT-training (all session pairs) \n"
-CUDA_VISIBLE_DEVICES=5 python ../main_chime9_mixit_half_all.py \
+CUDA_VISIBLE_DEVICES=0 python ../main_chime9_mixit_half_all.py \
 --save_path exps/seanet_chime9_mixit_all_half_ft \
 --data_list /net/midgar/work/nitsu/work/chime9/data/datalist_train \
 --backbone seanet \
